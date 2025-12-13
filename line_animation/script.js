f721c14d81a41b51a2260ff5c06fc99624f38e78
uchicha1511 <notla21bcs119@iiitkottayam.ac.in>
2025-12-13 12:20:57 +0530

    hover_animation

---
+++ line_animation/script.js
@@ -0,0 +1,32 @@
+gsap.from("#img1",
+    
+    {
+        dealay: 0.5,
+        duration: 1,
+        y: -150,
+        opacity: 0,
+        // ease: "power2.out"
+    }
+)
+
+gsap.from("#img2",
+    
+    {
+        dealay: 0.5,
+        duration: 1,
+        x: 100,
+        opacity: 0,
+        // ease: "power2.out"
+    }
+)
+
+gsap.from("#img3",
+    
+    {
+        dealay: 0.5,
+        duration: 1,
+        y: 100,
+        opacity: 0,
+        // ease: "power2.out"
+    }
+)
