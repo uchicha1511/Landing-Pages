cdc0b1283ab6f16289428e0980fb87407e3554e5
uchicha1511 <notla21bcs119@iiitkottayam.ac.in>
2025-12-14 15:14:16 +0530

    infinite_loading

--- GSAP/proj1/script.js
+++ GSAP/proj1/script.js
@@ -1,24 +1,27 @@
-gsap.to(".imgcontainer",{
+var tl = gsap.timeline(
+    {repeat:-1,
+    repeatDelay:-0.5,
+}
+);
+
+tl.to(".imgcontainer",{
     ease: Expo.easeInOut,
     duration: 2,
     width: "100%",
     stagger: 2
-})
-
-gsap.to(".text h1",{
+},'a')
+.to(".text h1",{
     ease: Expo.easeInOut,
     
     delay:0.5,
     stagger: 2,
     top: "0%",
-})
-
-
-gsap.to(".text h1",{
+},'a')
+.to(".text h1",{
     delay: 2.5,
     ease: Expo.easeInOut,
     
     
     stagger: 2,
     top: "-100%",
-})
+},'a')
